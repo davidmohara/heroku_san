0021da923a2b41476a676b3ab8a4b0b72d7a1864
David O'Hara <david@davidohara.net>
2011-02-24 23:16:14 -0600

    Updated db command

--- lib/heroku_san/tasks.rb
+++ lib/heroku_san/tasks.rb
@@ -229,11 +229,11 @@ end
 namespace :db do
   task :pull do
     each_heroku_app do |name, app, repo|
-      sh "heroku pgdumps:capture --app #{app}"
-      dump = `heroku pgdumps --app #{app}`.split("\n").last.split(" ").first
+      sh "heroku pgbackups:capture --app #{app}"
+      dump = `heroku pgbackups --app #{app}`.split("\n").last.split(" ").first
       sh "mkdir -p #{Rails.root}/db/dumps"
       file = "#{Rails.root}/db/dumps/#{dump}.sql.gz"
-      url = `heroku pgdumps:url --app #{app} #{dump}`.chomp
+      url = `heroku pgbackups:url --app #{app} #{dump}`.chomp
       sh "wget", url, "-O", file
       sh "rake db:drop db:create"
       sh "gunzip -c #{file} | #{Rails.root}/script/dbconsole"
